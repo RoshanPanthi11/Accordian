f91e62a48321624006b5201a6033fa1ce07216a8
RoshanPanthi11 <roshanpanthi13@gmail.com>
2024-08-17 14:51:17 +0545

    First changes

---
+++ src/components/accordian/data.js
@@ -0,0 +1,32 @@
+const data =[
+    {
+        id:'1',
+        question:"what are the accordian Components?",
+        answer:
+        "Accordian components are user interface element used for organization"
+    
+    },
+    {
+        id:'2',
+        question:"what are the accordian Components?",
+        answer:
+        "Accordian components are user interface element used for organization"
+    
+    },
+    {
+        id:'3',
+        question:"what are the accordian Components?",
+        answer:
+        "Accordian components are user interface element used for organization"
+    
+    },
+    {
+        id:'4',
+        question:"what are the accordian Components?",
+        answer:
+        "Accordian components are user interface element used for organization"
+    
+    },
+
+];
+export default data;
